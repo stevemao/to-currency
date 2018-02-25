2b80f4c84896a84250bb71db9b76d79bf250c965
Steve Mao <maochenyan@gmail.com>
2018-02-26 10:58:27 +1100

    brackets

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 module.exports = (number, locale, currencyCode) => {
 	if (process.env.NODE_ENV === 'development') {
-		if (typeof number !== 'number')
-		throw Error('First argument must be number')
+		if (typeof number !== 'number') {
+			throw Error('First argument must be number')
+		}
 	}
 
 	return number.toLocaleString(locale, { style: 'currency', currency: currencyCode })
