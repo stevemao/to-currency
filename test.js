65dfb93eb4a5e20f8bf5bbfc847c21cd6ec3e764
Steve Mao <maochenyan@gmail.com>
2018-05-25 11:20:23 +1000

    add rounding tests

--- test.js
+++ test.js
@@ -7,3 +7,11 @@ test('convert to AUD in Australia', () => {
 test('convert to AUD in the US', () => {
   expect(toCurrency(14104.25, 'en-US', 'AUD')).toBe('A$14,104.25')
 })
+
+test('should round down to two decimals', () => {
+  expect(toCurrency(14104.25432, 'en-US', 'AUD')).toBe('A$14,104.25')
+})
+
+test('should round up to two decimals', () => {
+  expect(toCurrency(14104.2553, 'en-US', 'AUD')).toBe('A$14,104.26')
+})
